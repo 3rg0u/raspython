9e999bb77c83912f806c4458d939bfa905d67430
0xf2 <nqaisme@icloud.com>
2025-03-03 17:37:24 +0700

    rq__

--- emulator/interrupt.py
+++ emulator/interrupt.py
@@ -9,7 +9,6 @@ _sensor_90 = 22
 _sensor_45 = 26
 _relay_90 = 17
 _relay_45 = 27
-_lcd = LCD1602()
 _btn_start = 23
 _btn_stop = 24
 _led = 25
@@ -69,39 +68,44 @@ def led():
 def clsf():
     global _state, _sensor_90, _sensor_45, _cnt_90, _cnt_45, _relay_90, _relay_45
     while True:
+        # if 90's product is encountered
+        # => raise up relay 90c, down relay 45c
         if _state and GPIO.input(_sensor_90) == 0:
             print("valid 90cm")
             _cnt_90 += 1
             GPIO.output(_relay_90, GPIO.HIGH)
-            time.sleep(3)
-            GPIO.output(_relay_45, GPIO.LOW)
+            time.sleep(3)  # assume product takes 3s to exit
+            GPIO.output(_relay_90, GPIO.LOW)
+
         elif _state and GPIO.input(_sensor_45) == 0:
             print("valid 45cm")
             _cnt_45 += 1
             GPIO.output(_relay_45, GPIO.HIGH)
-            time.sleep(5)
-            GPIO.output(_relay_90, GPIO.LOW)
-        elif _state:
-            print("not valid")
-            GPIO.output(_relay_90, GPIO.LOW)
+            time.sleep(5)  # assume product takes 5s to exit
             GPIO.output(_relay_45, GPIO.LOW)
-            time.sleep(7)
         time.sleep(0.1)
 
 
-def main():
-    global _lcd, _init, _cnt_90, _cnt_45
+def lcd_config():
+    _lcd = LCD1602()
+    global _init, _cnt_90, _cnt_45
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
         temp, humi = read_sensor()
         _lcd.clear()
+        # if system has not actived yet, display default msg
         if not _init:
             _lcd.write_string("DEM SAN PHAM D")
             _lcd.set_cursor(1, 0)
             _lcd.write_string(f"{temp:.2f}°C - {humi:.2f}%")
+        # else: display tempurature - humidity and product counts
         else:
             _lcd.write_string(f"{temp:.2f}°C - {humi:.2f}%")
-            _lcd.setdefault(1, 0)
-            _lcd.write_string(f"90c={_cnt_90}-45c={_cnt_45}")
+            _lcd.set_cursor(1, 0)
+            _lcd.write_string(f"90c={_cnt_90} - 45c={_cnt_45}")
         pygame.time.delay(200)
         _lcd.backlight_off()
         pygame.time.delay(50)
@@ -110,13 +114,20 @@ def main():
         _lcd.home()
         pygame.time.delay(50)
         time.sleep(2)
-        _lcd.clear()
 
 
 threading.Thread(target=start, daemon=True).start()
 threading.Thread(target=stop, daemon=True).start()
 threading.Thread(target=led, daemon=True).start()
 threading.Thread(target=clsf, daemon=True).start()
+threading.Thread(target=lcd_config).start()
+
+
+def main():
+    while True:
+        time.sleep(0.1)
+
+
 if __name__ == "__main__":
     try:
         main()
